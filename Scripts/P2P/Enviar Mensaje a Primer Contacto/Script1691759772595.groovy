a0f5b9e6b5e59ecab8bdc7bcd5ae6427f8bd8ded
Jose Salazar <josesalazarsg02@gmail.com>
2023-08-15 09:34:21 -0400

    first commit

---
+++ Scripts/P2P/Enviar Mensaje a Primer Contacto/Script1691759772595.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.cybercapitalparnerscorp.venchat.develop')
+
+Mobile.waitForElementPresent(findTestObject('Object Repository/Main Menu - Chats/primer contacto'), 0)
+
+Mobile.tap(findTestObject('Main Menu - Chats/primer contacto'), 0)
+
+Mobile.tap(findTestObject('chat P2P/caja de texto'), 0)
+
+Mobile.sendKeys(findTestObject('chat P2P/caja de texto'), 'hola - auto')
+
+Mobile.tap(findTestObject('chat P2P/boton enviar'), 0)
+
